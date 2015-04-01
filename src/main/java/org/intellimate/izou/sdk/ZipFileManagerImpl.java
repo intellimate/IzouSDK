364ae38564bc97b48ef7c1c07e9608e2be41ffff
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2015-04-02 00:12:44 +0200

    updated sdk

---
+++ src/main/java/org/intellimate/izou/sdk/ZipFileManagerImpl.java
@@ -0,0 +1,22 @@
+package org.intellimate.izou.sdk;
+
+import org.intellimate.izou.sdk.addon.ZipFileManager;
+import ro.fortsoft.pf4j.PluginWrapper;
+
+/**
+ * the ZipFileManager-Implementation used for the distribution
+ * @author LeanderK
+ * @version 1.0
+ */
+public class ZipFileManagerImpl extends ZipFileManager {
+    /**
+     * Constructor to be used by plugin manager for plugin instantiation.
+     * Your plugins have to provide constructor with this exact signature to
+     * be successfully loaded by manager.
+     *
+     * @param wrapper the PluginWrapper to assign the ZipFileManager to
+     */
+    public ZipFileManagerImpl(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+}
--- src/main/java/org/intellimate/izou/sdk/addon/ZipFileManager.java
+++ src/main/java/org/intellimate/izou/sdk/addon/ZipFileManager.java
@@ -1,26 +1,23 @@
 package org.intellimate.izou.sdk.addon;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import ro.fortsoft.pf4j.Plugin;
+import ro.fortsoft.pf4j.IzouPlugin;
 import ro.fortsoft.pf4j.PluginWrapper;
 
 /**
  * This class must be extended for an AddOn to work properly.
  * It is used to identify the zip Files as candidates for AddOns
  */
-public abstract class ZipFileManager extends Plugin {
-    private final Logger fileLogger = LogManager.getLogger(this.getClass());
-
+public abstract class ZipFileManager extends IzouPlugin {
 
     /**
      * Constructor to be used by plugin manager for plugin instantiation.
      * Your plugins have to provide constructor with this exact signature to
      * be successfully loaded by manager.
      *
-     * @param wrapper the PluginWrapper to assign the ZipFileManager to
+     * @param wrapper    the PluginWrapper to assign the ZipFileManager to
+     * @param sdkVersion the version of the izou sdk this plugin is using
      */
-    public ZipFileManager(PluginWrapper wrapper) {
-        super(wrapper);
+    public ZipFileManager(PluginWrapper wrapper, String sdkVersion) {
+        super(wrapper, sdkVersion);
     }
 }
