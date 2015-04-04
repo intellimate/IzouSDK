1e9a45b537519ae74ef4ea1bcc115ca120a0442e
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2015-04-04 22:02:04 +0200

    added EventController

---
+++ src/main/java/org/intellimate/izou/sdk/events/EventsController.java
@@ -0,0 +1,32 @@
+package org.intellimate.izou.sdk.events;
+
+import org.intellimate.izou.events.EventModel;
+import org.intellimate.izou.events.EventsControllerModel;
+import org.intellimate.izou.sdk.Context;
+import org.intellimate.izou.sdk.util.AddOnModule;
+
+/**
+ * represents an EventsController used for Controlling the dispatching of Events
+ * @author LeanderK
+ * @version 1.0
+ */
+public abstract class EventsController extends AddOnModule implements EventsControllerModel {
+
+    public EventsController(Context context, String ID) {
+        super(context, ID);
+    }
+
+    @Override
+    public boolean controlEventDispatcher(EventModel eventModel) {
+        if (eventModel.getType().equals(Event.NOTIFICATION))
+            return true;
+        return controlEvents(eventModel);
+    }
+
+    /**
+     * Controls whether the fired Event should be dispatched to all the listeners. This method should execute quickly
+     * @param eventModel the Event
+     * @return true if it should dispatch, false if not
+     */
+    public abstract boolean controlEvents(EventModel eventModel);
+}
