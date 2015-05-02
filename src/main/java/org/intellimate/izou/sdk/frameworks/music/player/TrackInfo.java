7cfc93e898e99789cb0f66d3b4038a15bd4809cf
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2015-05-02 16:23:38 +0200

    added uppdate method to TrackInfo

--- src/main/java/org/intellimate/izou/sdk/frameworks/music/player/TrackInfo.java
+++ src/main/java/org/intellimate/izou/sdk/frameworks/music/player/TrackInfo.java
@@ -1,9 +1,11 @@
 package org.intellimate.izou.sdk.frameworks.music.player;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.BiPredicate;
 
 /**
- * This class represents the Information about one track
+ * This class represents the Information about one track, this class is immutable
  * @author LeanderK
  * @version 1.0
  */
@@ -93,5 +95,66 @@ public class TrackInfo {
      */
     public Optional<String> getId() {
         return Optional.of(id);
+    }
+
+    /**
+     * retruns true if any of these arguments is already existing and would be overwritten, otherwise retruns false.
+     * The method also returns true if everything is null except the albumCover-Format (maybe known in advance).
+     * @param name the name
+     * @param artist the artist
+     * @param album the album
+     * @param albumCover the album cover
+     * @param albumCoverFormat the album cover format
+     * @param id the id of the track
+     * @return true if new
+     */
+    public boolean isNew(String name, String artist, String album, byte[] albumCover, String albumCoverFormat, String id) {
+        if (name == null && artist == null && album == null && albumCover == null && id == null)
+            return true;
+        BiPredicate<String, String> compareStrings = (newString, oldString) ->
+                newString != null && (oldString == null || !newString.equals(oldString));
+        if (compareStrings.test(name, this.name)) {
+            return true;
+        }
+        if (compareStrings.test(artist, this.artist)) {
+            return true;
+        }
+        if (compareStrings.test(album, this.album)) {
+            return true;
+        }
+        if (compareStrings.test(id, this.id)) {
+            return true;
+        }
+        if (compareStrings.test(albumCoverFormat, this.albumCoverFormat)) {
+            return true;
+        }
+        if (albumCover != null) {
+            if (this.albumCover == null || !Arrays.equals(albumCover, this.albumCover))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * returns a trackinfo if some information was added and not overwritten
+     * @param name the name
+     * @param artist the artist
+     * @param album the album
+     * @param albumCover the album cover
+     * @param albumCoverFormat the album cover format
+     * @param id the id of the track
+     * @return a trackinfo if some information was updated
+     */
+    public Optional<TrackInfo> update(String name, String artist, String album, byte[] albumCover, String albumCoverFormat, String id) {
+        if (isNew(name, artist, album, albumCover, albumCoverFormat, id))
+            return Optional.empty();
+        return Optional.of(new TrackInfo(
+                this.name == null? name : this.name,
+                this.artist == null? artist : this.artist,
+                this.album == null? album : this.album,
+                this.albumCover == null? albumCover : this.albumCover,
+                this.albumCoverFormat == null? albumCoverFormat : this.albumCoverFormat,
+                this.id == null? id : this.id
+        ));
     }
 }
