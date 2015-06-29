72d849e65a66850ab6c20cf7ae1e69c02f8e07f1
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2015-06-30 01:25:44 +0200

    better javadoc

--- src/main/java/org/intellimate/izou/sdk/frameworks/music/player/template/Player.java
+++ src/main/java/org/intellimate/izou/sdk/frameworks/music/player/template/Player.java
@@ -32,13 +32,13 @@ import java.util.function.Consumer;
  * use this class to actually play music.
  * <p>
  * <b>How to use it:</b><br>
- * Usage is pretty straightforward. Extend this class an override the abstract methods.
- * To register further commands, use the getCommandHandler() method in the constructor.
- * to start playing, use the PlayerController.startPlaying() methods, never call the play() method directly!.
- * Also, don't call the stopSound() method directly!.
+ * Usage is pretty straightforward. Extend this class an override the abstract methods.<br>
+ * To register further commands, use the getCommandHandler() method in the constructor.<br>
+ * To start playing, use the PlayerController.startPlaying() methods, never call the play() method directly!.
+ * Also, don't call the stopSound() method directly, use stopMusicPlayback().
  * All basic methods are implemented in this class,but to code special behaviour fell free to explore the other
  * classes and interfaces.<br>
- * Note: the playback automatically stops when the user left.
+ * Note: the playback automatically stops when the user left.<br>
  * </p>
  * @author LeanderK
  * @version 1.0
@@ -199,8 +199,10 @@ public abstract class Player<T> extends OutputPlugin<T> implements MusicProvider
     }
 
     /**
+     * stops the playing/indicates that the playing stopped.
+     * <p>
      * this method has no effect if runsInPlay is enabled in the constructor.<br>
-     * stops the playing
+     * </p>
      */
     public void stopMusicPlayback() {
         if (runsInPlay || !isPlaying)
